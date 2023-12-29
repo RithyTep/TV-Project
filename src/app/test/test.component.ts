f154f7cb2cb87f5da2b8f0837ed7923f1e969187
RithyTep <teprithy2020@gmail.com>
2023-12-29 11:12:00 +0700

    t

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,7 +9,31 @@ import data from './moj-report.json';
 export class TestComponent {
   selectedOperations: any;
   data: any;
+  D20T: any;
+  D12T: any;
+  foreignerArrayD20T: any;
+  foreignerArrayD12T: any;
+
   constructor() {
     this.data = data;
+    this.D20T = this.data
+      .filter((item: any) => item.urgent_obj?.key === 20)
+      .map((item: { national_application: { key: string } }) => ({
+        ...item,
+        national_application:
+          item.national_application?.key === 'foreigner'
+            ? item.national_application
+            : null,
+      }));
+
+    this.D12T = this.data
+      .filter((item: any) => item.urgent_obj?.key === 12)
+      .map((item: { national_application: { key: string } }) => ({
+        ...item,
+        national_application:
+          item.national_application?.key === 'foreigner'
+            ? item.national_application
+            : null,
+      }));
   }
 }
